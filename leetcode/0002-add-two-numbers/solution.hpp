93017a5b3234c8e90bf843a9959085bc7f83172e
resserops <resserops@gmail.com>
2025-10-18 10:33:58 +0800

    style: format C++ source files with clang-format

--- leetcode/0002-add-two-numbers/solution.hpp
+++ leetcode/0002-add-two-numbers/solution.hpp
@@ -1,14 +1,14 @@
 struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
         ListNode fake, *p{&fake};
         int carry = 0;
         while (l1 != nullptr || l2 != nullptr || carry != 0) {
