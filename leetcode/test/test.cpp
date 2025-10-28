790f2b582e0273dbf4e9525cd7e3eff40021db05
resserops <resserops@gmail.com>
2025-10-29 00:40:40 +0800

    build: add Ninja support to build.py

--- leetcode/test/test.cpp
+++ leetcode/test/test.cpp
@@ -1,15 +1,19 @@
 #include "test.hpp"
 
+#include <cctype>
 #include <filesystem>
 #include <ranges>
 
-std::string GetTestSuitePath(const char *file) {
-    std::filesystem::path path{file};
-    return path.parent_path() / "test_suite.yaml";
+std::string GetTestSuitePath() {
+    std::filesystem::path path{PROBLEM_PATH};
+    std::string_view suite{testing::UnitTest::GetInstance()->current_test_info()->test_suite_name()};
+    std::string_view num{
+        suite.substr(std::distance(suite.begin(), std::find_if(suite.begin(), suite.end(), ::isdigit)))};
+    return path / num / "test_suite.yaml";
 }
 
 std::string GetTestCaseName() {
     std::string name{testing::UnitTest::GetInstance()->current_test_info()->name()};
-    std::ranges::transform(name, name.begin(), [](unsigned char c) { return std::tolower(c); });
+    name[0] = std::tolower(name[0]);
     return name;
 }
